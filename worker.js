9036d278c7a0320039eafdc27bb0dead61b7d0a5
ErikBoesen <me@erikboesen.com>
2021-05-09 20:11:17 -0400

    Add getting meals in worker

--- worker.js
+++ worker.js
@@ -16,9 +16,28 @@ const getHalls = () => {
     return get('halls');
 }
 
+const getMealsToday = (hallId) => {
+    const date = new Date();
+    let month = String(date.getMonth());
+    if (month.length == 1) {
+        month = '0' + month;
+    }
+    let day = String(date.getDate());
+    if (day.length == 1) {
+        day = '0' + day;
+    }
+    return get('halls/' + hallId + '/meals', {
+        date: date.getFullYear() + '-' + month + '-' + day
+    });
+}
+
 const areServing = () => {
     getHalls().then((halls) => {
-        console.log(halls);
+        for (let { id } of halls) {
+            getMealsToday(id).then((meals) => {
+                console.log(meals);
+            });
+        }
     });
 }
 
