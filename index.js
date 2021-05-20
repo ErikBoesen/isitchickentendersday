5ddf3cbddcd059aa58c50ac201b3dd1f02c181aa
ErikBoesen <me@erikboesen.com>
2021-05-19 23:34:02 -0400

    Add static files

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(express.static('static'));
 
 app.get('/', function(req, res){
     res.render('index', {
