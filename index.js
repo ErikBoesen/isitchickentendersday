1d673b161501b7963161aff0df46b85af60e9a80
ErikBoesen <me@erikboesen.com>
2021-05-22 19:23:01 -0400

    Add knex setup

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
+const knex = require('knex');
+const app = express();
+
+const pg = knex({
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+});
 
 app.set('view engine', 'pug');
 app.set('views', './views');
