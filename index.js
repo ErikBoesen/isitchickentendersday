2fde037f9d9d631cde817d38ac2102a865866e51
ErikBoesen <me@erikboesen.com>
2021-05-23 19:25:33 -0400

    Add phone signup endpoint

--- index.js
+++ index.js
@@ -12,12 +12,19 @@ const pg = knex({
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
 app.use(express.static('static'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.render('index', {
         isItChickenTendersDay: false,
     });
+});
+app.post('/', function(req, res) {
+    const phone = req.body.phone.replace(/[^0-9]/g, '');
+    res.sendStatus(StatusCodes.OK);
 });
 
 app.listen(process.env.PORT || 3000);
