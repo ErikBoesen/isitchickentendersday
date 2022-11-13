b0c6e1bfcb00700f9453d550d3e35e0af4b586e6
ErikBoesen <me@erikboesen.com>
2022-11-12 20:17:16 -0500

    Implement querying of API

--- scripts.js
+++ scripts.js
@@ -1,17 +1,66 @@
 const API_ROOT = 'https://api.yalemenus.com/';
 
+const elem = {
+    answer: document.getElementById('answer'),
+    conjunction: document.getElementById('conjunction'),
+}
+
 const get = (url, params) => {
-    return fetch(API_ROOT + url + '?' + new URLSearchParams({
-        params,
-    })).then((response) => {
+    return fetch(API_ROOT + url + '?' + new URLSearchParams(params)).then((response) => {
         return response.json();
     }).catch(function (error) {
         console.log(error);
     });
 }
 
-get('halls', {}).then((data) => {
-    console.log(data);
+
+const getToday = () => {
+    const date = new Date();
+    let month = String(date.getMonth());
+    if (month.length === 1) {
+        month = '0' + month;
+    }
+    let day = String(date.getDate());
+    if (day.length === 1) {
+        day = '0' + day;
+    }
+    return date.getFullYear() + '-' + month + '-' + day;
+};
+
+const getLunch = (meals) => {
+    for (let meal of meals) {
+        if (meal.name === 'Lunch') {
+            return meal;
+        }
+    }
+    return null;
+};
+
+// Check an arbitrary college's menus as they are generally all the same
+const COLLEGE_ID = 'GH';
+
+const areServing = () => {
+    return get('halls/' + COLLEGE_ID + '/meals', {date: getToday()}).then((meals) => {
+        // Chicken tenders appear to only be served at lunch
+        let lunch = getLunch(meals);
+        if (lunch) {
+            get('meals/' + lunch.id + '/items').then((items) => {
+                for (let item of items) {
+                    if (item.name.toLowerCase().includes('chicken tenders')) {
+
+                    }
+                }
+            });
+        }
+        return false;
+    });
+};
+
+areServing().then((serving) => {
+    if (serving) {
+        elem.answer.textContent = 'Yes';
+        elem.conjunction.textContent = 'And';
+    }
 });
 
 /*
